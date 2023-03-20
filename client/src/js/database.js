b0db57cc8045661ae536ea9a20e52655e60dc0d1
Alister Porteous <alisterporteous@hotmail.com>
2023-03-20 13:05:50 -0400

    added logic to accept and add content to db, added logic to get all content from database

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,34 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  try{
+    console.log('Put to db');
+    const contentDb = await openDB('jate', 1);
+    const tx = contentDb.transaction('jate', 'readwrite');
+    const store = tx.objectStore('jate');
+    const request = store.put({id: 1, value: content});
+    const result = await request;
+    console.log('Saved to DB', result);
+  } catch (err) {
+console.error('putDb not implemented');
+  }
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  try{
+    console.log('Get from db');
+    const contentDb = await openDB('jate', 1);
+    const tx = contentDb.transaction('jate', 'readonly');
+    const store = tx.objectStore('jate');
+    const request = store.getAll();
+    const result = await request;
+    console.log('result.value', result);
+    return result?.value;
+  } catch (err) {
+console.error('getDb not implemented');
+  }
+};
 
 initdb();
